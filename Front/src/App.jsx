b215afec72120d52f1af44c8e6e007c00ad08256
Ieremia Andronic <contact.ierem@gmail.com>
2024-08-04 10:25:12 +0200

    reorganize pages

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Login from "./pages/Login";
 import User from "./pages/User";
 
