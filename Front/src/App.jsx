465a5f861a760791129a22a3cc1b41c6141a6203
Ieremia Andronic <contact.ierem@gmail.com>
2024-08-14 22:55:23 +0200

    Add : -home page -Header -Footer

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
-import Home from "./pages/home";
-import Login from "./pages/Login";
-import User from "./pages/User";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import User from "./pages/User/User";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/user" element={<User />} />
+          <Route path="*" element={""} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ Front/src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+import logo from "/src/assets/img/argentBankLogo.png";
+import "./style.css";
+
+export default function Header() {
+  return (
+    <>
+      <nav className="main-nav">
+        <Link className="main-nav-logo" to="/">
+          <img
+            className="main-nav-logo-image"
+            src={logo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
