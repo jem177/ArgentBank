d17a06981ec99f6f31102e93ac45b21a17fadeb0
Ieremia Andronic <contact.ierem@gmail.com>
2024-08-16 17:19:49 +0200

    add user page

--- Front/src/pages/Login/Login.jsx
+++ Front/src/pages/Login/Login.jsx
@@ -1,3 +1,30 @@
+import "./style.css";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label>Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label>Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label>Remember me</label>
+            </div>
+
+            <button className="sign-in-button">Sign In</button>
+          </form>
+        </section>
+      </main>
+    </>
+  );
 }
